e802a0c10b2e96691c4b31bc1db67a0fe251b152
Jan Trejbal <jan.trejbal@gmail.com>
2016-06-10 16:47:13 +0200

    Refactor DockerNetworkManager

---
+++ src/DockerNetworkManager.js
@@ -0,0 +1,99 @@
+const Gtk = imports.gi.versions.Gtk = '3.0';
+
+const Lang = imports.lang;
+const Mainloop = imports.mainloop;
+
+const Main = imports.ui.main;
+const NetworkManager = imports.gi.NetworkManager;
+
+const DockerNetworkManager = new Lang.Class({
+	Name : 'DockerNetworkManager',
+
+	_init : function(extensionName, matchRegExp) {
+		this._extensionName = extensionName;
+		this._matchRegExp = matchRegExp;
+
+		this._nAttempts = 0;
+		this._ethDevices = {};
+		this._checkDevices();
+	},
+
+	_checkDevices : function() {
+		if(this._timeoutId){
+			Mainloop.source_remove(this._timeoutId);
+			this._timeoutId = null;
+		}
+
+		let _network = Main.panel.statusArea.aggregateMenu._network;
+		if (_network) {
+			if (!_network._client || !_network._settings) {
+				// Shell not initialised completely wait for max of
+				// 100 * 1 sec
+				if (this._nAttempts++ < 100) {
+					this._timeoutId = Mainloop.timeout_add(1000, Lang.bind(this, this._checkDevices));
+				}
+			}
+			else {
+				let _devices = _network._devices.wired.devices;
+
+				for ( var i = 0; i < _devices.length; i++) {
+					if (_devices[i]) {
+						this._deviceAdded(_devices[i]._getDescription(), _devices[i]);
+					}
+				}
+
+				_devices.watch('length', function (property, from, to) {
+					let _network = Main.panel.statusArea.aggregateMenu._network;
+					let _devices = _network._devices.wired.devices;
+
+					_devices.forEach(function (device, i) {
+						this._deviceAdded(device._getDescription(), device);
+					}.bind(this));
+				}.bind(this));
+			}
+		}
+	},
+
+	_deviceAdded : function(deviceDescription, device) {
+		if ( !this._matchRegExp.test(deviceDescription)) {
+			return;
+		}
+
+		let _this = this;
+
+		this._ethDevices[deviceDescription] = new Object();
+		if(this._ethDevices[deviceDescription].timeoutId){
+			Mainloop.source_remove(this._ethDevices[deviceDescription].timeoutId);
+			this._ethDevices[deviceDescription].timeoutId = null;
+		}
+
+		log(this._extensionName + ' hide: ' + deviceDescription);
+		device.item.actor.watch('visible', function (property, from, to) {
+			if (to) {
+				log(this._extensionName + ' rehide: ' + deviceDescription);
+				device.item.actor.visible = false;
+			}
+		});
+
+		device.item.actor.visible = false;
+		this._ethDevices[deviceDescription].device = device;
+	},
+
+	_deviceRemoved : function(deviceDescription, device) {
+		log(this._extensionName + ' show: ' + deviceDescription);
+		device.item.actor.unwatch('visible');
+
+		device.item.actor.visible = true;
+		delete this._ethDevices[deviceDescription];
+	},
+
+	destroy : function() {
+		Main.panel.statusArea.aggregateMenu._network._devices.wired.devices.unwatch('length');
+
+		for ( var deviceDescription in this._ethDevices) {
+			if (this._ethDevices.hasOwnProperty(deviceDescription)) {
+				this._deviceRemoved(deviceDescription, this._ethDevices[deviceDescription].device);
+			}
+		}
+	}
+});
--- src/extension.js
+++ src/extension.js
@@ -19,16 +19,10 @@
 const Gtk = imports.gi.versions.Gtk = '3.0';
 
 const Config = imports.misc.config;
-const Lang = imports.lang;
-
-const Main = imports.ui.main;
-
-const St = imports.gi.St;
-const NetworkManager = imports.gi.NetworkManager;
-const Mainloop = imports.mainloop;
 
 // Other javascript files in the docker-integration@jan.trejbal.gmail.com directory are accesible via Extension.<file name>
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
+const DockerNetworkManager = Extension.imports.DockerNetworkManager;
 
 //const Menu = Extension.imports.menu;
 //const Panel = Extension.imports.panel;
@@ -40,101 +34,21 @@ const Extension = imports.misc.extensionUtils.getCurrentExtension();
 let extensionName = Extension.dir.get_basename();
 let matchRegExp = /^Ethernet \(veth[a-z0-9]+\)$|^$/i;
 
-const DockerNetworkManager = new Lang.Class({
-	Name : 'DockerNetworkManager',
-	
-	_init : function() {
-		this._nAttempts = 0;
-		this._ethDevices = {};
-		this._checkDevices();
-	},
+let _instance_dockerNetworkManager;
 
-	_checkDevices : function() {
-		if(this._timeoutId){
-			Mainloop.source_remove(this._timeoutId);
-			this._timeoutId = null;
-		}
-		
-		let _network = Main.panel.statusArea.aggregateMenu._network;
-		if (_network) {
-			if (!_network._client || !_network._settings) {
-				// Shell not initialised completely wait for max of
-				// 100 * 1 sec
-				if (this._nAttempts++ < 100) {
-					this._timeoutId = Mainloop.timeout_add(1000, Lang.bind(this, this._checkDevices));
-				}
-			}
-			else {
-				let _devices = _network._devices.wired.devices;
+function createDockerNetworkManager() {
+	_instance_dockerNetworkManager = new DockerNetworkManager.DockerNetworkManager(extensionName, matchRegExp);
+}
 
-				for ( var i = 0; i < _devices.length; i++) {
-					if (_devices[i]) {
-						this._deviceAdded(_devices[i]._getDescription(), _devices[i]);
-					}
-				}
+function destroyDockerNetworkManager() {
+	_instance_dockerNetworkManager.destroy();
+	_instance_dockerNetworkManager = null;
+}
 
-				_devices.watch('length', function (property, from, to) {
-					let _network = Main.panel.statusArea.aggregateMenu._network;
-					let _devices = _network._devices.wired.devices;
-
-					_devices.forEach(function (device, i) {
-						this._deviceAdded(device._getDescription(), device);
-					}.bind(this));
-				}.bind(this));
-			}
-		}
-	},
-
-	_deviceAdded : function(deviceDescription, device) {
-		if ( !matchRegExp.test(deviceDescription)) {
-			return;
-		}
-		
-		let _this = this;
-
-		this._ethDevices[deviceDescription] = new Object();
-		if(this._ethDevices[deviceDescription].timeoutId){
-			Mainloop.source_remove(this._ethDevices[deviceDescription].timeoutId);
-			this._ethDevices[deviceDescription].timeoutId = null;
-		}
-
-		log(extensionName + ' hide: ' + deviceDescription);
-		device.item.actor.watch('visible', function (property, from, to) {
-			if (to) {
-				log(extensionName + ' rehide: ' + deviceDescription);
-				device.item.actor.visible = false;
-			}
-		});
-
-		device.item.actor.visible = false;
-		this._ethDevices[deviceDescription].device = device;
-	},
-
-	_deviceRemoved : function(deviceDescription, device) {
-		log(extensionName + ' show: ' + deviceDescription);
-		device.item.actor.unwatch('visible');
-
-		device.item.actor.visible = true;
-		delete this._ethDevices[deviceDescription];
-	},
-
-	destroy : function() {
-		Main.panel.statusArea.aggregateMenu._network._devices.wired.devices.unwatch('length');
-
-		for ( var deviceDescription in this._ethDevices) {
-			if (this._ethDevices.hasOwnProperty(deviceDescription)) {
-				this._deviceRemoved(deviceDescription, this._ethDevices[deviceDescription].device);
-			}
-		}
-	}
-});
-
-let _instance;
 function enable() {
-	_instance = new DockerNetworkManager();
+	createDockerNetworkManager();
 }
 
 function disable() {
-	_instance.destroy();
-	_instance = null;
+	destroyDockerNetworkManager();
 }
